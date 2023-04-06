c504fd8cb3e83de9260abbc20526b501d3b4f7ee
Jose Carlos <josecarlos.soliscano@gmail.com>
2023-04-06 02:49:24 -0600

    commit 06 04 2023

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,27 +1,89 @@
-const cursos = [
-    {
-        titulo: 'Javascript Moderno Guia Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React - La ',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js - B',
-        tecnologia: 'Node.js',
-    },
-    {
-        titulo: 'ReactJS Avanzado - ',
-        tecnologia: 'React',
-    }
-];
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/Usuario');
+const Proyecto = require('../models/Proyecto');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: 'variables.env'});
+
+
+// Crea y firma un JWT
+const crearToken = (usuario, secreta, expiresIn) => {
+    console.log(usuario);
+    const { id, email } = usuario;
+
+    return jwt.sign( { id, email }, secreta, {expiresIn} );
+}
+
 
 const resolvers = {
     Query: {
-        obtenerCursos: () => cursos,
 
-        obtenerTecnologia: () => cursos,
+    },
+    Mutation: {
+        crearUsuario: async (_, {input}) => {
+            const { email, password } = input;
+
+            const existeUsuario = await Usuario.findOne({email});
+
+            // si el usuario existe
+            if(existeUsuario) {
+                throw new Error('El  usuario ya esta registrado');
+            }
+
+            try {
+
+                // Hashear password
+                const salt = await bcryptjs.getSalt(10);
+                input.password = await bcryptjs.hash(password, salt);
+
+                // Registrar nuevo usuaurio
+                const nuevoUsuario = new Usuario(input);
+                //console.log(nuevoUsuario);
+
+                nuevoUsuario.save();
+                return "Usuario Creado Correctamente";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        autenticarUsuario: async (_, {input}) => {
+            const {email, password} = input;
+
+            // Si el usuario existe
+            const existeUsuario = await Usuario.findOne({email});
+
+            // si el usuario no existe
+            if(!existeUsuario) {
+                throw new Error('El  usuario no existe');
+            }
+
+            // Si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+
+            if(!passwordCorrecto) {
+                throw new Error('Password Incorrecto');
+            }
+
+            // Dar acceso a la app
+
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '2hr')
+            }
+        },
+        nuevoProyecto: async (_, {input}) => {
+
+            try {
+                const proyecto = new Proyecto(input);
+
+                // almacenarlos en la BD
+                const resultado = await proyecto.save();
+
+                return resultado;
+
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -1,20 +1,68 @@
-const { gql } = require('apollo-server');
+const  {  gql } = require('apollo-server');
 
-const typeDefs = gql `
-
-    type Curso {
-        titulo: String
-        tecnologia: String
+const typeDefs = gql`
+    type Token {
+        token: String
     }
 
-    type Tecnologia {
-        tecnologia: String
+    type Proyecto {
+        nombre: String
+        id: ID
+    }
+
+    type Tarea {
+        nombre: String
+        id: ID
+        proyecto: String
+        estado: Boolean 
     }
 
     type Query {
-        obtenerCursos : [Curso]
+        obtenerProyectos : [Proyecto]
 
-        obtenerTecnologia: [Tecnologia]
+        obtenerTareas(input: ProyectoIDInput) : [Tarea]
+    }
+
+    input ProyectoIDInput {
+        proyecto: String!
+    }
+
+    input UsuarioInput {
+        nombre: String!
+        email: String!
+        password: String!
+    }
+
+    input AutenticarInput {
+        email: String!
+        password: String!
+    }
+
+    input ProyectoInput {
+        nombre: String!
+    }
+
+    input TareaInput {
+        nombre: String!
+        proyecto: String
+    }
+
+
+    type Mutation {
+
+        # Usuarios
+        crearUsuario(input: UsuarioInput) : String
+        autenticarUsuario(input: AutenticarInput ) : Token
+
+        # Proyectos
+        nuevoProyecto(input: ProyectoInput ) : Proyecto
+        actualizarProyecto(id : ID!,  input: ProyectoInput) : Proyecto
+        eliminarProyecto(id: ID!) : String
+
+        # Tareas
+        nuevaTarea(input: TareaInput) : Tarea
+        actualizarTarea(id: ID!, input: TareaInput, estado: Boolean ) : Tarea
+        eliminarTarea(id: ID!) : String
     }
 
 `;
