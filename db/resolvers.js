f5c1b708f37ea861f492f06764c6b76106f06b96
Jose Carlos <josecarlos.soliscano@gmail.com>
2023-04-13 10:21:17 -0600

    commit 13 04 2023

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 const Proyecto = require('../models/Proyecto');
+const Tarea = require('../models/Tarea');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({path: 'variables.env'});
 
@@ -16,7 +17,16 @@ const crearToken = (usuario, secreta, expiresIn) => {
 
 const resolvers = {
     Query: {
+        obtenerProyectos: async (_, {}, ctx) => {
+            const proyectos = await Proyecto.find({ creador: ctx.usuario.id });
 
+            return proyectos;
+        },
+        obtenerTareas: async (_, {input}, ctx) => {
+            const tarwas = await Tarea.find({ creador: ctx.usuario.id }).where('proyecto').equals(input.proyecto);
+
+            return tareas;
+        }
     },
     Mutation: {
         crearUsuario: async (_, {input}) => {
@@ -69,10 +79,14 @@ const resolvers = {
                 token: crearToken(existeUsuario, process.env.SECRETA, '2hr')
             }
         },
-        nuevoProyecto: async (_, {input}) => {
+        nuevoProyecto: async (_, {input}, ctx )=> {
+
 
             try {
                 const proyecto = new Proyecto(input);
+
+                // asociar el creador
+                proyecto.creador = ctx.usuario.id;
 
                 // almacenarlos en la BD
                 const resultado = await proyecto.save();
@@ -83,6 +97,90 @@ const resolvers = {
                 console.log(error);
             }
 
+        },
+        actualizarProyecto: async (_, {id, input}, ctx) =>  {
+            // Revisar si el proyecto existe o no
+            let proyecto = await Proyecto.findById(id);
+
+            if (!proyecto) {
+                throw new Error('Proyecto no encontrado');
+            }
+
+            // Revisar si la persona que trata de editarlo es el creador
+            if(proyecto.creador.toString() != ctx.usuario.id) {
+                throw new Error('No tienes permiso para editar');
+            }
+
+            // Guardar el proyecto
+            proyecto = await Proyecto.findOneAndUpdate({_id: id}, input, {new: true});
+            return proyecto;
+        },
+        eliminarProyecto: async (_, {id}, ctx) =>  {
+            // Revisar si el proyecto existe o no
+            let proyecto = await Proyecto.findById(id);
+
+            if (!proyecto) {
+                throw new Error('Proyecto no encontrado');
+            }
+
+            // Revisar si la persona que trata de editarlo es el creador
+            if(proyecto.creador.toString() != ctx.usuario.id) {
+                throw new Error('No tienes permiso para editar');
+            }
+
+            // Eliminar
+            await Proyecto.findOneAndDelete({ _id :id });
+
+            return "Proyecto Eliminado";
+        },
+        nuevaTarea: async (_, {input}, ctx) => {
+            try {
+                const tarea = new tarea(input);
+                tarea.creador = ctx.usuario.id;
+                const resultado = await tarea.save();
+                return resultado;
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        actualizarTarea: async (_, {id, input, estado}, ctx) => {
+            // Si la tarea existe o no
+            let tarea = await Tarea.findById(id);
+
+            if(!tarea) {
+                throw new Error('Tarea no encontrada');
+            }
+
+            // Si la persona que edita es el creador
+            if(proyecto.creador.toString() != ctx.usuario.id) {
+                throw new Error('No tienes permiso para editar');
+            }
+
+            // Asignar estado
+            input.estado = estado;
+
+            // Guardar y retornar la tarea
+            tarea = await Tarea.findOneAndUpdate({ _id : id }, input, { new: true });
+
+            return tarea;
+        },
+        eliminarTarea: async (_, {id}, ctx) => {
+            // Si la tarea existe o no
+            let tarea = await Tarea.findById(id);
+
+            if(!tarea) {
+                throw new Error('Tarea no encontrada');
+            }
+
+            // Si la persona que edita es el creador
+            if(proyecto.creador.toString() != ctx.usuario.id) {
+                throw new Error('No tienes permiso para editar');
+            }
+            
+            // Eliminar
+            await Tarea.findOneAndDelete({_id: id});
+
+            return "Tarea Eliminada";
         }
     }
 }
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const {ApolloServer} = require('apollo-server');
-
+const jwt = require('jsonwebtoken')
+require('dotenv').config('variables.env')
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 
 const conectarDB = require('./config/db');
 
+
 // Conectar a la BD
 conectarDB();
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context: ({requ}) => {
+        const token = req.headers['authorization'] || '';
+        if (token) {
+            try {
+                const usuario = jwt.verify(token, process.env.SECRETA);
+                return {
+                    usuario
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+});
+ 
 server.listen().then( ({url}) => {
     console.log(`Servidor listo en la URL ${url}`);
 } )
